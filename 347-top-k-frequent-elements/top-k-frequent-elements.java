d3dfde58f31c18ffc4856f5169b75d8cfe6b3506
Aditya Bhanarkar <86182801+asbhanarkar@users.noreply.github.com>
2025-04-20 15:09:29 +0530

    Time: 11 ms (92.25%) | Memory: 46.1 MB (83.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -25,9 +25,6 @@ class Solution {
             if(bucket[i] != null){
                 for(int val: bucket[i]){
                     res[counter++] = val;
-                    if(counter == k){
-                        return res;
-                    }
                 }
                 
             }
