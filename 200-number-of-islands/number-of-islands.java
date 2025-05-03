1cb0cf1f5a05896f5dfa2c62b7044bc9839ad98e
Aditya Bhanarkar <86182801+asbhanarkar@users.noreply.github.com>
2025-05-03 16:30:10 +0530

    Time: 2 ms (99.77%) | Memory: 49.4 MB (71.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,30 +1,30 @@
 class Solution {
 
-    public void dfs(int n, int m, char grid[][], boolean vis[][]){
+    public void dfs(int n, int m, char grid[][]){
         int i = grid.length;
         int j = grid[0].length;
-        if(n < 0 || m < 0 || n >= i || m >= j || grid[n][m] == '0' || vis[n][m] == true ) {
+        if(n < 0 || m < 0 || n >= i || m >= j || grid[n][m] == '0' ) {
             return;
         }
         
-        vis[n][m] = true;
-        dfs(n -1, m, grid, vis);
-        dfs(n + 1, m, grid, vis);
-        dfs(n, m - 1,grid, vis);
-        dfs(n, m + 1, grid, vis);
+        grid[n][m] = '0';
+
+        dfs(n -1, m, grid);
+        dfs(n + 1, m, grid);
+        dfs(n, m - 1,grid);
+        dfs(n, m + 1, grid);
         
         return;
     }
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean vis[][] = new boolean[n][m];
         int count = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
+                if(grid[i][j] == '1'){
                     count++;
-                    dfs(i, j, grid, vis);
+                    dfs(i, j, grid);
                 }
             }
         }
